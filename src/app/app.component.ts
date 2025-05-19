0dfdf329bfbdbf8df65b68373ea941057308ae63
olinicker <linicker.ostroski@alunos.ifsuldeminas.edu.br>
2025-05-19 14:54:06 -0300

    ngSwitch e Switch directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { StructuralDirectiveNgifVsIfComponent } from './structural-directive-ngif-vs-if/structural-directive-ngif-vs-if.component';
-import { StructuralDirectiveNgForVsForComponent } from './structural-directive-ng-for-vs-for/structural-directive-ng-for-vs-for.component';
+import { StructuralDirectiveNgswitchVsSwitchComponent } from './structural-directive-ngswitch-vs-switch/structural-directive-ngswitch-vs-switch.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StructuralDirectiveNgForVsForComponent],
+  imports: [RouterOutlet, StructuralDirectiveNgswitchVsSwitchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
